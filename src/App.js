fcb275a283f2340861cfa3955a3edd376bde7e05
Nicolas Corbalan <outreef@hotmail.com>
2021-06-13 15:39:51 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
 import React from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 const App = () => {
-  return <div>Hello React</div>;
+  return (
+    <div>
+      <Router>
+        <Route>
+          Hello React
+        </Route>
+      </Router>
+    </div>
+  );
 };
 
 export default App;
