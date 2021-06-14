6a8051605d78c939cd46aeaaebb2a49ad133389d
Nicolas Corbalan <outreef@hotmail.com>
2021-06-14 11:50:02 +0200

    adding navigate

--- src/App.js
+++ src/App.js
@@ -1,13 +1,33 @@
 import React from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import { css } from '@emotion/css';
+
+
+import Admin from './Admin/Admin';
+import Products from './Products/Products';
+
+const AppStyles = css`
+  margin: 50px;
+  width: 380px;
+  .Container {
+    background: #1d1f26;
+    border: 4px solid #9580ff;
+    border-radius: 6px;
+    padding: 25px;
+  }
+`
 
 const App = () => {
   return (
-    <div>
+    <div className={AppStyles}>
       <Router>
-        <Route>
-          Hello React
-        </Route>
+        <div className='Container'>
+        <Routes>
+          <Route path="/" element={<Products />} />
+          <Route path="/admin" element={<Admin />} />
+          <Route path="*" element={<Navigate to="/" />} />
+        </Routes>
+        </div>
       </Router>
     </div>
   );
---
+++ src/Products/Products.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { css } from '@emotion/css';
+
+const ProductsStyles = css`
+  display: flex;
+  flex-direction: column;
+  .Logo {
+    width: 125px;
+    margin: 0 auto 25px;
+  }
+`;
+
+const Products = () => {
+  return (
+    <div className={ProductsStyles}>
+      <img src="/assets/img/logo.svg" alt="Ultimate Burgers" className="Logo" />
+      Products
+    </div>
+  )
+}
+
+export default Products
